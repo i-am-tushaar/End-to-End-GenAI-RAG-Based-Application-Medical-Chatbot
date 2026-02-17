e2f0db18a0f69b51c758a6e6dcc0dbd904a8adde
Tushar <tushaar@example.com>
2026-02-17 16:38:40 +0530

    Completed project setup by configuring setup.py for modular packaging and installing dependencies

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Tushar Choudhary",
+    author_email="tushar.dataexpert@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
